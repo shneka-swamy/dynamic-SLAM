43ecd14546fbd7dcc308d65fce80afb87bed7d83
shneka-swamy <shneka.swamy@gmail.com>
2023-09-14 03:14:14 -0600

    determine the parameters

--- determine_parameter_values.py
+++ determine_parameter_values.py
@@ -1,23 +1,38 @@
 import argparse
 from pathlib import Path
+import json
 import numpy as np
+import re
+import math
 
 def arg_parser():
     parser = argparse.ArgumentParser(description='Determine parameter values')
-    parser.add_value('--data_folder', type=Path, help='Path to data folder')
-    parser.add_value('--deter-eps-value', action='store_true', help='Determine best eps value for DBSCAN')
-    parser.add_value('--deter-temp-value', action='store_true', help='Determine best number of tracker value')
+    parser.add_argument('--data_folder', type=Path, help='Path to data folder')
+    parser.add_argument('--deter-eps-value', action='store_true', help='Determine best eps value for DBSCAN')
+    parser.add_argument('--deter-temp-value', action='store_true', help='Determine best number of tracker value')
     return parser.parse_args()
 
 def read_data_file(data_folder):
-    # Read numpy data
-    data = np.load(data_folder)
-    print(data)
+    with open(data_folder/"stats.json") as f:
+        d = json.load(f)
+        print(d['rmse'])
+    
+    timestamps = np.load(data_folder/"timestamps.npy")
+    print(timestamps.shape[0])    
 
 def main():
     args = arg_parser()
-    read_data_file(args.data_folder)
 
+    # Find all the folders inside the data folder
+    folders = [f for f in args.data_folder.iterdir() if f.is_dir()]
+    choose_pattern = ['walking', 'sitting']
+
+    for pattern in choose_pattern:
+        for folder in folders:
+            if pattern in str(folder):
+                folder_name_list = str(folder).split('_')
+                if folder_name_list[-1].isdigit() and folder_name_list[-2].isdigit():
+                    read_data_file(folder)
 
 if __name__ == "__main__":
     main()
