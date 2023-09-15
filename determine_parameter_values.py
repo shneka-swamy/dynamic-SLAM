8872c6dbbee3e4a876fb3e251fa04aa238e02051
shneka-swamy <shneka.swamy@gmail.com>
2023-09-15 06:27:03 -0600

    changed the run file

--- determine_parameter_values.py
+++ determine_parameter_values.py
@@ -17,12 +17,15 @@ def arg_parser():
 def read_data_file(data_folder):
     # Read stat inside zip file and print rmse, without extracting the zip file
 
+    print(f'Processing {data_folder}')
     with zipfile.ZipFile(data_folder, 'r') as zip_ref:
         with zip_ref.open('stats.json') as f:
             d = json.load(f)
         
         with zip_ref.open('timestamps.npy') as f:
             timestamps = np.load(f)
+
+        print(f'rmse: {d["rmse"]:.5f}')
 
         return d['rmse'], timestamps.shape[0]
 
@@ -94,40 +97,12 @@ def plot_graph_rmse(df_store):
     plt.tight_layout()
     plt.savefig('temp_vs_rmse.pdf')
 
-
-def main():
-    args = arg_parser()
-
-    # Find all the folders inside the data folder
-    folders = [f for f in args.data_folder.iterdir() if f.is_file()]
-    choose_pattern = ['walking_xyz', 'sitting_rpy']
-    choose_pattern=["walking_xyz", "sitting_xyz", "sitting_halfsphere" ,"walking_rpy", "walking_static", "walking_halfsphere"]
-
-    #df_store = pd.DataFrame(columns=['pattern', 'eps', 'rmse'])
-    df_store = pd.DataFrame(columns=['pattern', 'rmse', 'no_kf'])
-
-    for pattern in choose_pattern:
-        df = pd.DataFrame(columns=['eps','runNo', 'rmse', 'no_kf'])
-        for i, folder in enumerate(folders):
-            # make sure pattern in folder and extension is zip
-            if pattern in str(folder) and str(folder).endswith('.zip'):
-                folder_name_list = str(folder.stem).split('_')
-                if folder_name_list[-1].isdigit() and folder_name_list[-2].isdigit():
-                    rmse, no_kf = read_data_file(folder)
-                    df = df.append({'eps': folder_name_list[-2], 'runNo': folder_name_list[-1], 'rmse': rmse, 'no_kf': no_kf}, ignore_index=True)
-        
-
-        # Find the best eps value
-        #df_store = find_best_eps(df, df_store, pattern)
-
-        # Print for the best template
-        pattern, rmse, no_kf = find_best_temp(df, pattern)
-        df_store = df_store.append({'pattern': pattern, 'rmse': rmse, 'no_kf': no_kf}, ignore_index=True)
-
+def get_pattern(df_store):
     # print rsme and no_kf as list
     patternList = df_store['pattern'].to_numpy()
     rsmeList = df_store['rmse'].to_numpy()
     no_kfList = df_store['no_kf'].to_numpy()
+    
 
     # print list with brackets and comma separated
     def printList(list):
@@ -138,6 +113,65 @@ def main():
 
     print(df_store)
 
+def main():
+    args = arg_parser()
+
+    # Find all the folders inside the data folder
+    folders = [f for f in args.data_folder.iterdir() if f.is_file()]
+    choose_pattern = ['walking_xyz', 'sitting_rpy']
+    choose_pattern=["walking_xyz", "sitting_xyz", "sitting_halfsphere" ,"walking_rpy", "walking_static", "walking_halfsphere"]
+
+    #df_store = pd.DataFrame(columns=['pattern', 'eps', 'rmse'])
+    df_store_ate = pd.DataFrame(columns=['pattern', 'rmse', 'no_kf'])
+    df_store_tr = pd.DataFrame(columns=['pattern', 'rmse', 'no_kf'])
+    df_store_rr = pd.DataFrame(columns=['pattern', 'rmse', 'no_kf'])
+
+    for pattern in choose_pattern:
+        df_ape = pd.DataFrame(columns=['eps','runNo', 'rmse', 'no_kf'])
+        df_rr = pd.DataFrame(columns=['eps','runNo', 'rmse', 'no_kf'])
+        df_tr = pd.DataFrame(columns=['eps','runNo', 'rmse', 'no_kf'])
+
+        for i, folder in enumerate(folders):
+            # make sure pattern in folder and extension is zip
+            if pattern in str(folder) and str(folder).endswith('.zip'):
+                folder_name_list = str(folder.stem).split('_')
+                typeStr = folder_name_list[-1]
+                seqNo = folder_name_list[-2]
+                epsNo = folder_name_list[-3]
+                if epsNo and seqNo:
+                    rmse, no_kf = read_data_file(folder)
+                    
+                    if 'rr' in typeStr:
+                        df_rr = df_rr.append({'eps': folder_name_list[-2], 'runNo': folder_name_list[-1], 'rmse': rmse, 'no_kf': no_kf}, ignore_index=True)
+                    elif 'tr' in typeStr:
+                        df_tr = df_tr.append({'eps': folder_name_list[-2], 'runNo': folder_name_list[-1], 'rmse': rmse, 'no_kf': no_kf}, ignore_index=True)
+                    elif 'ape' in typeStr:
+                        df_ape = df_ape.append({'eps': folder_name_list[-2], 'runNo': folder_name_list[-1], 'rmse': rmse, 'no_kf': no_kf}, ignore_index=True)
+                    else:
+                        print("Error: typeStr not found")
+                        assert False, f"{typeStr} not found"        
+
+        # Find the best eps value
+        #df_store = find_best_eps(df, df_store, pattern)
+
+        # Print for the best template
+        pattern, rmse, no_kf = find_best_temp(df_ape, pattern)
+        df_store_ate = df_store_ate.append({'pattern': pattern, 'rmse': rmse, 'no_kf': no_kf}, ignore_index=True)
+
+        pattern, rmse, no_kf = find_best_temp(df_tr, pattern)
+        df_store_tr = df_store_tr.append({'pattern': pattern, 'rmse': rmse, 'no_kf': no_kf}, ignore_index=True)
+
+        pattern, rmse, no_kf = find_best_temp(df_rr, pattern)
+        df_store_rr = df_store_rr.append({'pattern': pattern, 'rmse': rmse, 'no_kf': no_kf}, ignore_index=True)
+
+    
+    # Print the pattern, rmse and no_kf as list
+    print("ATE")
+    get_pattern(df_store_ate)
+    print("TR")
+    get_pattern(df_store_tr)
+    print("RR")
+    get_pattern(df_store_rr)
 
     #plot_graph(df_store)
     # Plot a graph of eps vs rmse for each pattern
